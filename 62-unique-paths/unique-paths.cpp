183ff465481b4d47018b39a60c119637a9c1f2f9
RoysrX <121816905+RoysrX@users.noreply.github.com>
2026-05-26 17:57:28 +0530

    Time: 2 ms (20.66%) | Memory: 9.5 MB (28.29%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int m, int n, vector<vector<int>>& dp){
-         if(m == 0 && n == 0){
+        if(m == 0 && n == 0){
             return 1;
         }
         if(m<0 || n<0){
@@ -14,6 +14,29 @@ public:
         int left = solve(m, n-1, dp);
 
         return dp[m][n] = (up+left);
+    }
+
+    int solveByTabu(int m, int n, vector<vector<int>> &dp){
+        dp[0][0] = 1;
+        for(int i=0; i<m-1; i++){
+            for(int j=0; j<n-1; j++){
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+
+                if(up > 0){
+                    up = dp[i-1][j];
+                }
+                if(left > 0){
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = (up+left);
+            }
+        }
+        return dp[m-1][n-1];
     }
 
     int uniquePaths(int m, int n){
