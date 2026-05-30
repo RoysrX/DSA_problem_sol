71a93afc369a7df452dfeb8594b3a4b1e72c03fb
RoysrX <121816905+RoysrX@users.noreply.github.com>
2026-05-30 10:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (58.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
 
-    int sol(int n, int m, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(n==0 && m==0) return grid[0][0];
-        if(n < 0 || m < 0 || n >= grid.size() || m >= grid[0].size()) return 10000000;
-        if(dp[n][m] != -1) return dp[n][m];
+    // int sol(int n, int m, vector<vector<int>> &grid, vector<vector<int>> &dp){
+    //     if(n==0 && m==0) return grid[0][0];
+    //     if(n < 0 || m < 0 || n >= grid.size() || m >= grid[0].size()) return 10000000;
+    //     if(dp[n][m] != -1) return dp[n][m];
 
-        int up = grid[n][m] + sol(n-1, m, grid, dp);
-        int left = grid[n][m] + sol(n, m-1, grid, dp);
+    //     int up = grid[n][m] + sol(n-1, m, grid, dp);
+    //     int left = grid[n][m] + sol(n, m-1, grid, dp);
 
-        return dp[n][m] = min(up, left);
+    //     return dp[n][m] = min(up, left);
+    // }
+    
+    int solByTabu(int n, int m, vector<vector<int>> &grid, vector<vector<int>> &dp){
+        for(int i =0; i<=n-1; i++){
+            for(int j=0 ;j <=m-1; j++){
+                if(i==0 && j ==0) dp[i][j] = grid[0][0];
+                else{
+                    int up=INT_MAX;
+                    int left=INT_MAX;
+                    if(i>0) up = grid[i][j] + dp[i-1][j];
+                    if(j>0) left = grid[i][j] + dp[i][j-1];
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
+
 
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
@@ -18,6 +35,6 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        return sol(n-1, m-1, grid, dp);
+        return solByTabu(n, m, grid, dp);
     }
 };
